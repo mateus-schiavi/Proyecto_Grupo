f7141621a51d6064c4b794438c93696b872d2471
Mateus Schiavi <mtsschiavi@gmail.com>
2023-08-16 15:15:36 -0300

    C

--- array.js
+++ array.js
@@ -1,4 +1,4 @@
-import { alegriaArray, tristeArray, enojonArray, miedoArray } from './main.js'; 
+import { alegriaArray, tristeArray, enojonArray, miedoArray } from "./main.js";
 
 const botonAlegria = document.querySelector("#alegria-button");
 const botonTristeza = document.querySelector("#tristeza-button");
@@ -8,41 +8,54 @@ const botonEnojo = document.querySelector("#enojo-button");
 const displayContainer = document.querySelector("#container");
 
 function displayQuote(quote) {
-   
-    displayContainer.innerHTML = '';
+  displayContainer.innerHTML = "";
 
-    const quoteElement = document.createElement("div");
-    quoteElement.classList.add("quote");
+  const quoteElement = document.createElement("div");
+  quoteElement.classList.add("quote");
 
-    const quoteContent = document.createElement("p");
-    quoteContent.textContent = quote.contenido;
+  const quoteContent = document.createElement("p");
+  quoteContent.textContent = quote.contenido;
 
-    const quoteImage = document.createElement("img");
-    quoteImage.src = quote.Img;
-    quoteImage.alt = quote.name;
+  const quoteImage = document.createElement("img");
+  quoteImage.src = quote.Img;
+  quoteImage.alt = quote.name;
 
-    quoteElement.appendChild(quoteContent);
-    quoteElement.appendChild(quoteImage);
+  quoteElement.appendChild(quoteContent);
+  quoteElement.appendChild(quoteImage);
 
-    displayContainer.appendChild(quoteElement);
+  displayContainer.appendChild(quoteElement);
 }
 
 botonAlegria.addEventListener("click", () => {
-    const randomIndex = Math.floor(Math.random() * alegriaArray.length);
-    displayQuote(alegriaArray[randomIndex]);
+  const randomIndex = Math.floor(Math.random() * alegriaArray.length);
+  displayQuote(alegriaArray[randomIndex]);
 });
 
 botonTristeza.addEventListener("click", () => {
-    const randomIndex = Math.floor(Math.random() * tristeArray.length);
-    displayQuote(tristeArray[randomIndex]);
+  const randomIndex = Math.floor(Math.random() * tristeArray.length);
+  displayQuote(tristeArray[randomIndex]);
 });
 
 botonMiedo.addEventListener("click", () => {
-    const randomIndex = Math.floor(Math.random() * miedoArray.length);
-    displayQuote(miedoArray[randomIndex]);
+  const randomIndex = Math.floor(Math.random() * miedoArray.length);
+  displayQuote(miedoArray[randomIndex]);
 });
 
 botonEnojo.addEventListener("click", () => {
-    const randomIndex = Math.floor(Math.random() * enojonArray.length);
-    displayQuote(enojonArray[randomIndex]);
+  const randomIndex = Math.floor(Math.random() * enojonArray.length);
+  displayQuote(enojonArray[randomIndex]);
 });
+/*
+const enlacesContainer = document.getElementById('enlaces');
+
+  enojonArray.forEach(enlaces => {
+    enlacesContainer = document.createElementNS('a');
+    enlacesElement.textContent = enlaces.texto;
+    enlacesElement.href = enlaces.url;
+    enlacesElement.target = '_blank';
+    enlacesContainer.appendChild(enlacesElement);
+
+  })
+
+*/
+
